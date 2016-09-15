bd874328391386d168ad0b9f3c5bc57eef1e9e9d
Barend <bvanrijn@users.noreply.github.com>
2016-09-15 18:09:55 +0200

    200% more easter eggs

--- rules_bot.py
+++ rules_bot.py
@@ -1,10 +1,11 @@
 import logging
 from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
+
 
 updater = Updater(token='123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
 dispatcher = updater.dispatcher
@@ -22,18 +23,27 @@ def rules(bot, update):
     else:
         bot.sendMessage(chat_id=update.message.chat_id, text='Hmm. You\'re not in a python-telegram-bot group, and I don\'t know the rules around here.')
 
+def other(bot, update):
+    if update.message.chat.username == "pythontelegrambotgroup":
+        if any(ot in update.message.text for ot in ('off-topic', 'off topic', 'offtopic')):
+            bot.sendMessage(chat_id=update.message.chat_id, text="The off-topic group is [here](https://telegram.me/pythontelegrambottalk). Come join us!", disable_web_page_preview=True, parse_mode="Markdown", reply_to_message_id=update.message.message_id)
+
+    if update.message.chat.username == "pythontelegrambottalk":
+        if "sudo make me a sandwich" in update.message.text:
+            bot.sendMessage(chat_id=update.message.chat_id, text="Okay.", reply_to_message_id=update.message.message_id)
+        elif "make me a sandwich" in update.message.text:
+            bot.sendMessage(chat_id=update.message.chat_id, text="What? Make it yourself.", reply_to_message_id=update.message.message_id)
+
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
-    # Do `touch rules_bot.log` first!
-    with open('rules_bot.log', 'a') as logfile:
-        logfile.write('Update "%s" caused error "%s"' % (update, error))
-        logfile.close()
 
 start_handler = CommandHandler('start', start)
 rules_handler = CommandHandler('rules', rules)
+other_handler = MessageHandler([Filters.text], other)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(rules_handler)
+dispatcher.add_handler(other_handler)
 dispatcher.add_error_handler(error)
 
 updater.start_polling()
