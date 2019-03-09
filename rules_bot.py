e5c0f2d4180ba07ef2a3ab3138fa8275cdc5f16e
Jasmin Bom <jsmnbom@gmail.com>
2019-03-09 14:08:05 +0100

    Only look for github patterns in text messages

--- rules_bot.py
+++ rules_bot.py
@@ -238,7 +238,7 @@ def main():
     # therefore we catch everything and do regex ourselves
     # This should probably be in another dispatcher group
     # but I kept getting SystemErrors...
-    github_handler = MessageHandler(Filters.all, github)
+    github_handler = MessageHandler(Filters.text, github)
     forward_faq_handler = MessageHandler(Filters.regex(r'(?i).*#faq.*'), forward_faq)
 
     dispatcher.add_handler(rate_limit_tracker_handler, group=-1)
