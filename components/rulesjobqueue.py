7a9fba3f55cb2ec81f493bc8150e203dcfaba91e
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2023-05-02 20:46:26 +0200

    pre-commit

--- components/rulesjobqueue.py
+++ components/rulesjobqueue.py
@@ -7,8 +7,8 @@ class RulesJobQueue(JobQueue):
     async def stop(self, wait: bool = True) -> None:
         """Declines all join requests and stops the job queue. That way, users will know that
         they have to apply to join again."""
-        # We loop instead of `asyncio.gather`-ing to minimize the rist of timeouts & flood limits
+        # We loop instead of `asyncio.gather`-ing to minimize the risk of timeouts & flood limits
         for job in self.jobs():
-            if job.name.startswith("JOIN_TIMEOUT"):
+            if job.name and job.name.startswith("JOIN_TIMEOUT"):
                 await job.run(self.application)
         await super().stop(wait)
