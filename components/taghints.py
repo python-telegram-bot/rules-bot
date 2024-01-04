27f75e44de0324ccdb3d8da32ca68ebf28440fbc
Luca Bellanti <luca.bellanti@gmail.com>
2024-01-04 21:35:21 +0100

    line length is a thing

--- components/taghints.py
+++ components/taghints.py
@@ -244,12 +244,13 @@ _TAG_HINTS: Dict[str, Dict[str, Any]] = {
     },
     "compat": {
         "message": (
-            "{query} You seem to be using the new version (&gt;=20.0) of <code>python-telegram-bot</code>"
-            " but your code is written for an older and deprecated version (&lt;=13.15).\nPlease update"
-            " your code to the new v20 by reading the release notes and the transition guide linked below."
-            "You can also install a version of PTB that is compatible with your code base, "
-            "but please note that the library has undergone significant changes in v20 and the "
-            "older version is not supported anymore and may be broken."
+            "{query} You seem to be using the new version (&gt;=20.0) of"
+            "<code>python-telegram-bot</code> but your code is written for an older and "
+            "deprecated version (&lt;=13.15).\nPlease update your code to the new v20 by reading"
+            " the release notes and the transition guide linked below.\nYou can also install a "
+            "version of PTB that is compatible with your code base, but please note that the "
+            "library has undergone significant changes in v20 and the older version is not "
+            "supported anymore and may be broken."
         ),
         "buttons": [
             [
