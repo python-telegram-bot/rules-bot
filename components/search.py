f934c7ed923a6aa181527278c869309e1388fa15
Hinrich Mahler <22366557+Bibo-Joshi@users.noreply.github.com>
2024-07-21 20:59:44 +0200

    Small fixes

--- components/search.py
+++ components/search.py
@@ -51,7 +51,7 @@ class Search:
         self._faq: List[FAQEntry] = []
         self._design_patterns: List[FRDPEntry] = []
         self.github = GitHub(auth=github_auth, user_agent=github_user_agent)
-        self._httpx_client = httpx.AsyncClient()
+        self._httpx_client = httpx.AsyncClient(headers=DEFAULT_HEADERS)
 
     async def initialize(
         self, application: Application[Any, Any, Any, Any, Any, JobQueue]
@@ -111,7 +111,7 @@ class Search:
         self.multi_search_combinations.cache_clear()  # pylint:disable=no-member
 
     async def _update_official_docs(self) -> None:
-        response = await self._httpx_client.get(url=OFFICIAL_URL, headers=DEFAULT_HEADERS)
+        response = await self._httpx_client.get(url=OFFICIAL_URL)
         official_soup = BeautifulSoup(response.content, "html.parser")
         for anchor in official_soup.select("a.anchor"):
             if "-" not in anchor["href"]:
@@ -177,7 +177,7 @@ class Search:
                     )
 
     async def update_readme(self) -> None:
-        response = await self._httpx_client.get(url=DOCS_URL, headers=DEFAULT_HEADERS)
+        response = await self._httpx_client.get(url=DOCS_URL, follow_redirects=True)
         readme_soup = BeautifulSoup(response.content, "html.parser")
         self._readme = []
 
@@ -195,7 +195,7 @@ class Search:
                 )
 
     async def update_wiki(self) -> None:
-        response = await self._httpx_client.get(url=WIKI_URL, headers=DEFAULT_HEADERS)
+        response = await self._httpx_client.get(url=WIKI_URL)
         wiki_soup = BeautifulSoup(response.content, "html.parser")
         self._wiki = []
 
@@ -216,9 +216,7 @@ class Search:
         self._wiki.append(WikiPage(category="Code Resources", name="Examples", url=EXAMPLES_URL))
 
     async def update_wiki_code_snippets(self) -> None:
-        response = await self._httpx_client.get(
-            url=WIKI_CODE_SNIPPETS_URL, headers=DEFAULT_HEADERS
-        )
+        response = await self._httpx_client.get(url=WIKI_CODE_SNIPPETS_URL)
         code_snippet_soup = BeautifulSoup(response.content, "html.parser")
         self._snippets = []
         for headline in code_snippet_soup.select(
@@ -232,7 +230,7 @@ class Search:
             )
 
     async def update_wiki_faq(self) -> None:
-        response = await self._httpx_client.get(url=WIKI_FAQ_URL, headers=DEFAULT_HEADERS)
+        response = await self._httpx_client.get(url=WIKI_FAQ_URL)
         faq_soup = BeautifulSoup(response.content, "html.parser")
         self._faq = []
         for headline in faq_soup.select("div#wiki-body h3"):
@@ -244,7 +242,7 @@ class Search:
             )
 
     async def update_wiki_design_patterns(self) -> None:
-        response = await self._httpx_client.get(url=WIKI_FRDP_URL, headers=DEFAULT_HEADERS)
+        response = await self._httpx_client.get(url=WIKI_FRDP_URL)
         frdp_soup = BeautifulSoup(response.content, "html.parser")
         self._design_patterns = []
         for headline in frdp_soup.select("div#wiki-body h3,div#wiki-body h2"):
